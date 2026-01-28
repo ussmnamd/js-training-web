885c07e3e735f83326fe7550916c173e09429747
ussmnamd <ussmnamd@gmail.com>
2026-01-28 16:44:19 +0500

    feat: add vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import './index.css'
 import { router } from './router/Router.tsx'
 import ErrorBoundary from './components/ErrorBoundary.tsx'
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ErrorBoundary>
       <RouterProvider router={router} />
+      <SpeedInsights />
     </ErrorBoundary>
   </StrictMode>,
 )
